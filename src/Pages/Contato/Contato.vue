854d03ca476e4f1f7ed443d9026ded11abfecd71
Rodrigo Silva <rodrigodamasceno115@gmail.com>
2021-08-04 11:23:18 -0400

    ajeitando o css

--- src/Pages/Contato/Contato.vue
+++ src/Pages/Contato/Contato.vue
@@ -5,18 +5,25 @@
           <div class="row">
             <div class="col-12 col-sm-6 col-lg-6 mb-5 mt-5">
               <div class="quadrado bg-dark">
-                  <div class="container-fluid border border-success">
-                      <div class="row">
-                          <div class="col-lg-5">
-                              <input type="text" placeholder="Nome">
+                  <div class="container-fluid container-height">
+                      <form action="" class="mt-5 border">
+                          <div class="row p-5">
+                              <div class="col">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" placeholder="Nome">
+                                    </div>
+                              </div>
+                              <div class="col">
+                                  <div class="form-group">
+                                      <input type="email" class="form-control" placeholder="E-mail">
+                                  </div>
+                              </div>
+                              <div class="w-100"></div>
+                              <div class="col mt-5">
+                                  <textarea class="form-control" rows="3" placeholder="Mensagem"></textarea>
+                              </div>
                           </div>
-                          <div class="col-lg-5">
-                              <input type="text" placeholder="E-mail">
-                          </div>
-                          <div class="col border">
-                              <input type="text-area" placeholder="Mensagem">
-                          </div>
-                      </div>
+                      </form>
                   </div>
               </div>
             </div>
@@ -37,5 +44,13 @@
     .quadrado{
         height: 700px;
         opacity: 0.5;
+    }
+
+    .container-height{
+        height: 100%;
+    }
+
+    .container-height form{
+        height: 100%;
     }
 </style>
